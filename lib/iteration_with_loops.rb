b60fef41e7e92d5b05b89ad2f1586a5af1eb678d
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-25 18:00:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,8 +8,8 @@ while count < src.length do
   index_src = 0
   while index_src < src[count].length do
     if src[count][index_src].class.to_s == 'String'
-      string = src[count][index_src]+ " "
-  index_src += 1
+      string += src[count][index_src]+ " "
+      index_src += 1
 end
 count += 1
 end
